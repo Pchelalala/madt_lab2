ad2e9bf12db2accdc5cb54153f1115a507398532
Pchela <lyudmilalittlepchela@gmail.com>
2022-10-12 11:02:16 +0300

    change project structure

---
+++ lib/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class TextFieldClass extends StatefulWidget {
+  const TextFieldClass({Key? key}) : super(key: key);
+
+  @override
+  _TextFieldState createState() => _TextFieldState();
+}
+
+class _TextFieldState extends State<TextFieldClass> {
+  final textController = TextEditingController();
+
+  int charLength = 0;
+
+  _onChanged(String value) {
+    setState(() {
+      charLength = value.length;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("Length = $charLength",
+                      style: const TextStyle(fontSize: 20))),
+              Container(
+                  width: 280,
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: textController,
+                    autocorrect: true,
+                    decoration:
+                    const InputDecoration(hintText: 'Enter Some Text Here'),
+                    onChanged: _onChanged,
+                  )),
+            ],
+          ),
+        ));
+  }
+}
