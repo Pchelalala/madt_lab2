02c27fe73e06e3860a1f348b3928d65606e8f879
Pchela <lyudmilalittlepchela@gmail.com>
2022-10-25 16:19:17 +0300

    change drop down button

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'drop_down_button.dart';
-
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -14,6 +12,12 @@ class _HomePageState extends State<HomePage> {
 
   int charLength = 0;
 
+  String dropdownValue = 'symbols';
+  var items = [
+    'symbols',
+    'words',
+  ];
+
   _onChanged(String value) {
     setState(() {
       charLength = value.length;
@@ -24,31 +28,45 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('What do you want to count?'),
-              const DropdownButtonExample(),
-              Container(
-                  width: 280,
-                  padding: const EdgeInsets.all(10.0),
-                  child: TextField(
-                    controller: textController,
-                    autocorrect: true,
-                    decoration:
-                    const InputDecoration(hintText: 'Enter Some Text Here'),
-                    onChanged: _onChanged,
-                  )),
-              ElevatedButton(
-                onPressed: () { },
-                child: const Text('Count!'),
-              ),
-              Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text("Length = $charLength",
-                      style: const TextStyle(fontSize: 20))),
-            ],
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text('What do you want to count?'),
+          DropdownButton(
+            value: dropdownValue,
+            icon: const Icon(Icons.keyboard_arrow_down),
+            items: items.map((String items) {
+              return DropdownMenuItem(
+                value: items,
+                child: Text(items),
+              );
+            }).toList(),
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue = newValue!;
+              });
+            },
           ),
-        ));
+          Container(
+              width: 280,
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                controller: textController,
+                autocorrect: true,
+                decoration:
+                    const InputDecoration(hintText: 'Enter Some Text Here'),
+                onChanged: _onChanged,
+              )),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Count!'),
+          ),
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Length = $charLength",
+                  style: const TextStyle(fontSize: 20))),
+        ],
+      ),
+    ));
   }
 }
